e021cdbb363d2041188cb129aac9a4d4eb994972
MD SAHIL RAZA <91309689+Sher-e-Haider@users.noreply.github.com>
2023-08-22 11:34:50 +0530

    second commit

--- controllers/authControl.js
+++ controllers/authControl.js
@@ -39,7 +39,7 @@ export const signup = async(req,res)=>{
      })
     // await newUser.save()
     res.send({
-        newUser,
+        result:newUser,
         token:generateToken(newUser),
         message:"User created successfully"
     })
